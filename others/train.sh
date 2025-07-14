0bf61e6c24843356199eb1c19888f3b7fe57c655
K-Y-Chen <48049555+K-Y-Chen@users.noreply.github.com>
2025-07-14 19:18:02 +0800

    Add files via upload
    other models

---
+++ others/train.sh
@@ -0,0 +1,4 @@
+python3 train_dual_copy.py --workers 8 --device 0 --batch 32 --data data/oesopstomach_fusion.yaml --img 640 --cfg models/detect/yolov9-l.yaml --weights ''  --hyp hyp.scratch-high.yaml --min-items 0 --epochs 500 --name CenterNet_base
+
+
+
