03a1b626750e19914b37501b610018e42711b2d7
K-Y-Chen <48049555+K-Y-Chen@users.noreply.github.com>
2025-08-06 14:41:58 +0800

    Update test.sh

--- others/test.sh
+++ others/test.sh
@@ -1,11 +1,13 @@
-python3 val.py --data oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/centernetbase/weights/best.pt' --batch-size 32 --modelname centernet --name centernetbase
+python3 val.py --data oesopstomach_fusion.yaml --weight 'path_to_centernet_weights' --batch-size 32 --modelname centernet --name centernet
 
-python3 val.py --data oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/retinanetbase/weights/best.pt' --batch-size 32 --modelname retinanet --name retinanetbase &&
+python3 val.py --data oesopstomach_fusion.yaml --weight 'path_to_retinanet_weights' --batch-size 32 --modelname retinanet --name retinanet &&
 
-python3 val.py --data oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/YOHObase/weights/best.pt' --batch-size 32 --modelname YOHO --name YOHObase &&
+python3 val.py --data oesopstomach_fusion.yaml --weight 'path_to_YOHO_weights' --batch-size 32 --modelname YOHO --name YOHO &&
 
-python3 val.py --data oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/efficientdetbase/weights/best.pt' --batch-size 32 --modelname efficientdet --name efficientdetbase &&
+python3 val.py --data oesopstomach_fusion.yaml --weight ''path_to_efficientdet_weights' --batch-size 32 --modelname efficientdet --name efficientdet &&
 
-python3 val.py --data oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/detrbase/weights/best.pt' --batch-size 32 --modelname detr --name detrbase && 
+python3 val.py --data oesopstomach_fusion.yaml --weight 'path_to_detr_weightst' --batch-size 32 --modelname detr --name detr && 
 
 echo done
+
+
