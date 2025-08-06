e25e6f4af385ce9b94242e4ee5c6a67ea2398d4b
K-Y-Chen <48049555+K-Y-Chen@users.noreply.github.com>
2025-08-06 14:42:22 +0800

    Update detect.sh

--- others/detect.sh
+++ others/detect.sh
@@ -1,11 +1,11 @@
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_centernet_weights'  --modelname centernet --name centernetbase && 
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_centernet_weights'  --modelname centernet --name centernet && 
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_retinanet_weights'  --modelname retinanet --name retinanetbase &&
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_retinanet_weights'  --modelname retinanet --name retinanet &&
 
 python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_YOHO_weights'  --modelname YOHO --name YOHObase &&
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_efficientdet_weights'  --modelname efficientdet --name efficientdetbase &&
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_efficientdet_weights'  --modelname efficientdet --name efficientdet &&
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_detr_weightst'  --modelname detr --name detrbase && 
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_detr_weightst'  --modelname detr --name detr && 
 
 echo done
