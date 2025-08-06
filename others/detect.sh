072d6ad3b43b28877467c7f2f377698893cc6baf
K-Y-Chen <48049555+K-Y-Chen@users.noreply.github.com>
2025-08-06 14:40:24 +0800

    Update detect.sh

--- others/detect.sh
+++ others/detect.sh
@@ -1,11 +1,11 @@
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/centernetbase/weights/best.pt'  --modelname centernet --name centernetbase && 
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_centernet_weights'  --modelname centernet --name centernetbase && 
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/retinanetbase/weights/best.pt'  --modelname retinanet --name retinanetbase &&
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_retinanet_weights'  --modelname retinanet --name retinanetbase &&
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/YOHObase/weights/best.pt'  --modelname YOHO --name YOHObase &&
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_YOHO_weights'  --modelname YOHO --name YOHObase &&
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/efficientdetbase/weights/best.pt'  --modelname efficientdet --name efficientdetbase &&
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_efficientdet_weights'  --modelname efficientdet --name efficientdetbase &&
 
-python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight '/workspace/OesopStomach/code/v5.others/runs/train/detrbase/weights/best.pt'  --modelname detr --name detrbase && 
+python3 detect.py --data ./data/oesopstomach_fusion.yaml --weight 'path_to_detr_weightst'  --modelname detr --name detrbase && 
 
 echo done
